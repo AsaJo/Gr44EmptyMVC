1e153b8539bf532c6c16f268d704df20403aa60e
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-11-03 11:25:40 +0100

    Add project files.

---
+++ Gr44EmptyMVC/Controllers/DarthVaderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gr44EmptyMVC.Controllers
+{
+    public class DarthVaderController : Controller
+    {
+        [HttpPost]
+        public IActionResult FeverCheck(double temp) // overload 
+        {
+            if (temp != 0)
+            {
+
+                ViewBag.Msg = Models.DarthVader.TestForFever(temp);
+                return View();
+            }
+            else
+            {
+                ViewBag.Msg = "Pls enter your temperature and then hit Submit";
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult FeverCheck()// overload
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Gr44EmptyMVC/Models/DarthVader.cs
@@ -0,0 +1,11 @@
+﻿namespace Gr44EmptyMVC.Models
+{
+    public class DarthVader
+    {
+        public static string TestForFever(double temperature)
+        {
+
+            return (temperature > 37) ? "Doctor Vader says you have fever...have a cookie" : "Doctor Vader says you don't have any fever...so no cookie";
+        }
+    }
+}
